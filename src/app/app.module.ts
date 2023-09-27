3a318b4b14223bff56f796eab89d8de6ed18ce9c
lukasZelinka <77839958+lukasZelinka@users.noreply.github.com>
2023-09-27 15:36:58 +0200

    navbar basic -  not responzive for now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonModule } from 'primeng/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { PrimengModule } from 'primeng';
+import { ToolbarModule } from 'primeng/toolbar';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarModule } from 'primeng/sidebar';
+import { MenuModule } from 'primeng/menu'; 
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ButtonModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ToolbarModule,
+    SidebarModule,
+    MenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
