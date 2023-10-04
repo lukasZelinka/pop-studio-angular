ee623e564ba346043e66edb15b884991b9c0427d
lukasZelinka <77839958+lukasZelinka@users.noreply.github.com>
2023-10-04 19:07:52 +0200

    checkpoint

--- src/app/videos/videos.component.ts
+++ src/app/videos/videos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef  } from '@angular/core';
 import { thumbnails, Thumbnail } from 'src/assets/data/data';
 
 @Component({
@@ -8,4 +8,60 @@ import { thumbnails, Thumbnail } from 'src/assets/data/data';
 })
 export class VideosComponent {
   thumbnails: Thumbnail[] = thumbnails
+  videoType = 'video/mp4';
+   @ViewChild('myVideo') videoPlayer!: ElementRef;
+
+
+   constructor() {
+    document.addEventListener('fullscreenchange', this.closeFullscreen.bind(this));
+  }
+
+   openFullscreen() {
+    const video = this.videoPlayer.nativeElement;
+
+    if (video.requestFullscreen) {
+      video.requestFullscreen().then(() => {
+        this.fullScreenSettings(video);
+      });
+    } else if (video.mozRequestFullScreen) { // Firefox
+      video.mozRequestFullScreen().then(() => {
+        this.fullScreenSettings(video);
+      });
+    } else if (video.webkitRequestFullscreen) { // Chrome, Safari, and Opera
+      video.webkitRequestFullscreen().then(() => {
+        this.fullScreenSettings(video);
+      });
+    } else if (video.msRequestFullscreen) { // IE/Edge
+      video.msRequestFullscreen().then(() => {
+        this.fullScreenSettings(video);
+      });
+    }
+  }
+
+  fullScreenSettings(video: HTMLVideoElement) {
+    video.currentTime = 0;
+    video.play();
+    video.muted = false;
+    video.volume = 1.0; 
+    video.loop = false
+  }
+  
+  closeFullscreen() {
+    const video = this.videoPlayer.nativeElement;
+    if ( !document.fullscreenElement )  {
+      video.muted = true;
+      video.controls = false;
+    }
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
--- src/assets/data/data.ts
+++ src/assets/data/data.ts
@@ -2,10 +2,9 @@ import { MenuItem } from "primeng/api";
 
 export interface Thumbnail {
   id: number;
-  imageUrl: string;
   name: string;
   desc: string;
-  path?: string;
+  path: string;
 }
 
  export const socialIcons: MenuItem[] = [
@@ -35,49 +34,33 @@ export interface Thumbnail {
 
  export const thumbnails: Thumbnail[] = [
     { id:1 , 
-      imageUrl:"https://primefaces.org/cdn/primeng/images/usercard.png",
       name: "prvy nazov", 
       desc: "prvy desc",
-      path: "../../assets/videos/pop-daniela.mp4",
+      path: "../../assets/videos/1.mp4",
     }
-      ,
-    // { id:2 , 
-    //   imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-XvvMFfz2sf5o2MNsvg8TYgY3YeL0YgJmmA&usqp=CAU", name: "druhy nazov", 
-    //   desc: "druhy desc",
-    // }
-    //   ,
-    //   { id:3 , 
-    //   imageUrl: "https://cdn-imgix.headout.com/tour/7064/TOUR-IMAGE/b2c74200-8da7-439a-95b6-9cad1aa18742-4445-dubai-img-worlds-of-adventure-tickets-02.jpeg?auto=format&w=480&h=205.7142857142857&q=90&fit=crop&ar=21%3A9", name: "druhy nazov", 
-    //   desc: "druhy desc"}
-    //   ,
-    //   { id:4 , 
-    //   imageUrl: "https://www.sdetmi.com/assets/Uploads/Event/79966/IMG-20230610-192821699-HDR.jpg", name: "druhy nazov", 
-    //   desc: "druhy desc"}
-    //   ,
-    //   { id:5 , 
-    //   imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-XvvMFfz2sf5o2MNsvg8TYgY3YeL0YgJmmA&usqp=CAU", name: "druhy nazov", 
-    //   desc: "druhy desc"}
-    //   ,
-    //   { id:6 , 
-    //   imageUrl:"https://primefaces.org/cdn/primeng/images/usercard.png",
-    //   name: "prvy nazov", 
-    //   desc: "prvy desc"}
-    //   ,
-    // { id:7, 
-    //   imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-XvvMFfz2sf5o2MNsvg8TYgY3YeL0YgJmmA&usqp=CAU", name: "druhy nazov", 
-    //   desc: "druhy desc"}
-    //   ,
-    //   { id:8 , 
-    //   imageUrl: "https://cdn-imgix.headout.com/tour/7064/TOUR-IMAGE/b2c74200-8da7-439a-95b6-9cad1aa18742-4445-dubai-img-worlds-of-adventure-tickets-02.jpeg?auto=format&w=480&h=205.7142857142857&q=90&fit=crop&ar=21%3A9", name: "druhy nazov", 
-    //   desc: "druhy desc"}
-    //   ,
-    //   { id:9 , 
-    //   imageUrl: "https://www.sdetmi.com/assets/Uploads/Event/79966/IMG-20230610-192821699-HDR.jpg", name: "druhy nazov", 
-    //   desc: "druhy desc"}
-    //   ,
-    //   { id:10, 
-    //   imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-XvvMFfz2sf5o2MNsvg8TYgY3YeL0YgJmmA&usqp=CAU", name: "druhy nazov", 
-    //   desc: "druhy desc"}
-    //   ,
-
+    ,
+     { id:2 , 
+      name: "2", 
+      desc: "prvy desc",
+      path: "../../assets/videos/2.mp4",
+    }
+    ,
+     { id:3 , 
+      name: "3", 
+      desc: "prvy desc",
+      path: "../../assets/videos/3.mp4",
+    }
+    ,
+     { id:4 , 
+      name: "4", 
+      desc: "prvy desc",
+      path: "../../assets/videos/4.mp4",
+    }
+    ,
+     { id:5 , 
+      name: "5", 
+      desc: "prvy desc",
+      path: "../../assets/videos/5.mp4",
+    }
+    
  ]
