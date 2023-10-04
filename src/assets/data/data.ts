9a72a906ba1e1f5d0e36b8eba49e2a6ffcc50688
lukasZelinka <77839958+lukasZelinka@users.noreply.github.com>
2023-10-04 19:23:40 +0200

    video gallery in progress

--- src/assets/data/data.ts
+++ src/assets/data/data.ts
@@ -62,5 +62,47 @@ export interface Thumbnail {
       desc: "prvy desc",
       path: "../../assets/videos/5.mp4",
     }
+    ,
+     { id:6 , 
+      name: "6", 
+      desc: "prvy desc",
+      path: "../../assets/videos/6.mp4",
+    }
+    ,
+     { id:7 , 
+      name: "7", 
+      desc: "prvy desc",
+      path: "../../assets/videos/7.mp4",
+    }
+    ,
+     { id:8 , 
+      name: "8", 
+      desc: "prvy desc",
+      path: "../../assets/videos/8.mp4",
+    }
+    ,
+     { id:9 , 
+      name: "9", 
+      desc: "prvy desc",
+      path: "../../assets/videos/9.mp4",
+    }
+    ,
+     { id:10 , 
+      name: "10", 
+      desc: "prvy desc",
+      path: "../../assets/videos/10.mp4",
+    }
+    ,
+     { id:11 , 
+      name: "11", 
+      desc: "prvy desc",
+      path: "../../assets/videos/11.mp4",
+    }
+    ,
+     { id:12 , 
+      name: "12", 
+      desc: "prvy desc",
+      path: "../../assets/videos/12.mp4",
+    }
     
  ]
